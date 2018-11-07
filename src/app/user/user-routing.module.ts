b135b608f967d4e822ef9122f19db1a45434545c
Kvoths <kvoths@gmail.com>
2018-11-07 20:44:32 +0100

    Añadido routing con acceso a login

---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+
+const userRoutes: Routes = [
+  { path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(userRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class UserRoutingModule { }
