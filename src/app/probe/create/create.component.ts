c428a2c4d0854de3b85cc03414c5928442b68635
carlos <carloslopez@nitsnets.com>
2019-04-27 13:03:49 +0200

    Llamada http para recuperar sondas

--- src/app/probe/create/create.component.ts
+++ src/app/probe/create/create.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { PeticionesService } from '../../services/peticiones.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
+  providers: [PeticionesService]
 })
 export class CreateComponent implements OnInit {
+  public probes;
 
-  constructor() { }
+  constructor(
+    private _peticionesService: PeticionesService
+  ) { }
 
   ngOnInit() {
+    this._peticionesService.getProbes().subscribe(
+      result => {
+        console.log(result);
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
